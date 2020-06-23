52a01937344e171c589e024486c495c497e1860c
neochan1 <neochan1@sina.com>
2020-06-23 14:57:01 +0800

    New file

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func sayHello(str string){
+	for i:=0;i<10;i++{
+		time.Sleep(100*time.Millisecond)
+		fmt.Println(str)
+	}
+}
+func main() {
+		go sayHello("World!")
+		sayHello("Hello,")
+		
+}
